ef7671eab7e0034c97d22bf8698845ba100ca055
gopalkashyap19 <gopal267kashyap@gmail.com>
2026-06-24 23:35:38 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,8 @@
+import pymysql
+def get_db_connection():
+    return pymysql.connect(
+        host="localhost",
+        user="root",
+        password="gopal123",
+        database="aichatbot"
+    )
